14dffa02f1be4afeb8bc3377d29c4b1cee4a94c5
razyoboy <razoreiei@gmail.com>
2022-08-29 12:37:03 +0700

    added more logic for testing with SonarQube

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorldConsole;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+
+        string fileName = "sampleQuotes.txt";
+        string pathText = Path.Combine(Environment.CurrentDirectory, fileName);
+
+        var lines = ReadFrom(pathText);
+        foreach (var line in lines)
+        {
+            Console.WriteLine(line);
+        }
+    }
+
+    static IEnumerable<string> ReadFrom(string file)
+    {
+        string? line;
+        using (var reader = File.OpenText(file))
+        {
+            while ((line = reader.ReadLine()) != null)
+            {
+                yield return line;
+            }
+        }
+    }
+}
+
